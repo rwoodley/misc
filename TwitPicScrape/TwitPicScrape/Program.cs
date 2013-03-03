7b006d091d2b9859eb53218d08d1a871db6b74b9
Bob Woodley <blwoodley@yahoo.com>
2013-03-02 22:21:05 -0600

    Initial commit

---
+++ TwitPicScrape/TwitPicScrape/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web.Script.Serialization;
+
+namespace TwitPicScrape
+{
+    class Program
+    {
+        static String _picturesDirectory = "pictures";
+        static HashSet<String> _knownNames = new HashSet<String>();
+        static void Main(string[] args)
+        {
+            foreach (String name in Directory.GetFiles(_picturesDirectory))
+                _knownNames.Add(name);
+
+            Dictionary<DateTime, string> nameDic = new Dictionary<DateTime, string>();
+            foreach (String name in _knownNames)
+                nameDic.Add(Directory.GetCreationTime(name), name);
+
+            foreach (var item in nameDic.OrderByDescending(x=>x.Key))
+                FindFollowers(
+                    String.Format("https://api.twitter.com/1/followers/ids.json?screen_name=@{0}", Path.GetFileNameWithoutExtension(item.Value)));
+        }
+        private static void FindFollowers(String url)
+        {
+            using (var webClient = new WebClient())
+            {
+                string html = webClient.DownloadString(url);
+                JavaScriptSerializer ser = new JavaScriptSerializer();
+                var dict = ser.Deserialize<Dictionary<string, object>>(html);
+                var ids = ((System.Collections.ArrayList)dict["ids"]);
+                foreach (int id in ids)
+                {
+                    String screenNameURL = String.Format("https://api.twitter.com/1/users/show.json?user_id={0}&include_entities=true", id);
+                    string html2 = webClient.DownloadString(screenNameURL);
+                    var dict2 = ser.Deserialize<Dictionary<string, object>>(html2);
+                    String screenName = (String) dict2["screen_name"];
+                    Console.WriteLine(id + ": " + screenName);
+
+                    if (_knownNames.Contains(screenName)) continue;
+
+                    String profilPicURL = String.Format("https://api.twitter.com/1/users/profile_image?screen_name={0}&size=original", screenName);
+                    MemoryStream ms = new MemoryStream(webClient.DownloadData(profilPicURL));
+                    System.Drawing.Image img = System.Drawing.Image.FromStream(ms);
+                    img.Save(_picturesDirectory + "\\" + screenName + ".jpg");
+                }
+            }
+        }
+    }
+}
